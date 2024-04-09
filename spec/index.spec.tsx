5f8b0b67fae5284974edff8473d76711beed7ad4
vitalyvitmens <otm_@tut.by>
2024-04-09 12:42:04 +0300

    chore(spec): создает test-suites

---
+++ spec/index.spec.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import { App } from '../src/App'
+
+describe('Планирование тестов', () => {
+  it.todo('Добавлять новые задачи.')
+  it.todo('Просматривать список всех задач.')
+  it.todo('Отмечать задачи как выполненные.')
+  it.todo('Удалять задачи.')
+  it.todo('Фильтровать задачи по стаусу (выполненные/невыполненные).')
+})
