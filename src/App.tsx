ff563633953cbc628578e7844803f84081ff1df3
vitalyvitmens <otm_@tut.by>
2024-04-22 16:44:53 +0300

    feat(src/components/Notifier.tsx): добавляет компонент Notifier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import { useState } from 'react'
 import { Input } from './components/Input'
 import { AddButton } from './components/AddButton'
+import { Notifier } from './components/Notifier'
 
 export const App = () => {
   const [Inputvalue, setInputValue] = useState('')
+  const [notifierOpen, setNotifierOpen] = useState(false)
+
   const handleAddButtonClick = () => {
     console.log('Button was clicked!')
   }
 
+  const handleNotifierClose = () => {
+    setNotifierOpen((prev) => !prev)
+  }
+
   return (
     <>
+      <Notifier
+        task="Любая задача"
+        open={notifierOpen}
+        onClose={handleNotifierClose}
+      />
       <Input value={Inputvalue} onChange={(val) => setInputValue(val)} />
       <AddButton onClick={handleAddButtonClick} />
+      <button onClick={() => setNotifierOpen((prev) => !prev)}>
+        Показать нотификацию
+      </button>
     </>
   )
 }
